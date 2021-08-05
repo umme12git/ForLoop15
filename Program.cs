6beb4f455bece6cc7691f5054ba8eda5459d10a0
umme <unnipa01@yahoo.com>
2021-08-05 07:57:46 -0500

    initial copy

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ForLoop15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 15; i++)
+            Console.WriteLine($"Value of i is {i}");
+        }
+    }
+}
